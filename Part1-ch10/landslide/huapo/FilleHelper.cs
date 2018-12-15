78f374691ac60f9459664582b3dc30170d42cb94
ybli <ybli@qq.com>
2018-12-15 11:04:15 +0800

    part1-ch10

---
+++ Part1-ch10/landslide/huapo/FilleHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace landside
+{
+    class FileHelper
+    {
+        public static List<MoniterPoint> Read(string filename)
+        {
+            var dataa = new List<MoniterPoint>();
+            MoniterPoint data = new landside.MoniterPoint();
+            try
+            {
+                var rd = new StreamReader(filename);
+                string line = rd.ReadLine();
+                int N = Convert.ToInt32(line);
+                for (int i = 0; i < N; i++)
+                {
+                    line = rd.ReadLine();
+                    var buf = line.Split(',');
+                    string name = buf[0];
+                    var mp = new MoniterPoint();
+                    mp.Name = name;
+                    int K = Convert.ToInt32(buf[1]);
+                    for (int j = 0; j < K; j++)
+                    {
+                        line = rd.ReadLine();
+                        Coordinate c = new Coordinate();
+                        c.Parse(line);
+                        mp.Data.Add(c);
+                    }
+                    dataa.Add(mp);
+                }
+                rd.Close();
+                return dataa;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+        public static void Write(string res, string file)
+        {
+            
+            try
+            {
+                var writer = new StreamWriter(file);
+                writer.Write(res);
+                writer.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
---
+++ Part1-ch10/landslide/huapo/MoniterPoint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace landside
+{
+    /// <summary>
+    /// 监测点
+    /// </summary>
+    public class MoniterPoint
+    {
+        public string Name { get; set; }
+        public List<Coordinate> Data { get; set; }
+
+        public MoniterPoint()
+        {
+            Name = string.Empty;
+            Data = new List<Coordinate>();
+        }
+
+        public Coordinate this[int i]
+        {
+            set { Data[i] = value; }
+            get { return Data[i]; }
+        }
+
+        public override string ToString()
+        {
+            string res = "测站    X（m）    Y（m)    \n";
+            foreach (var d in Data)
+            {
+                res += d.ToString() + "\n";
+            }
+            return res;
+        }
+    }
+}
