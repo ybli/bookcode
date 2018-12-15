24a3d81793d48603ada5ba4c31a9d3a3dd6a4671
ybli <ybli@qq.com>
2018-12-15 09:31:29 +0800

    part1-ch04

---
+++ Part1-Ch04/ShortPath/Edge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortPath
+{
+    /// <summary>
+    /// 边：由开始顶点、结束顶点、边长组成
+    /// </summary>
+    class Edge
+    {
+        public string Start { get; set; }
+        public string End { get; set; }
+        public double Length { get; set; }
+
+        public void Parse(string line)
+        {
+            var buf = line.Split(',');
+            Start = buf[0];
+            End = buf[1];
+            Length = Convert.ToDouble(buf[2]);
+        }
+
+        public override string ToString()
+        {
+            string line = $"{Start},{End},{Length}";
+            return line;
+        }
+    }
+}
