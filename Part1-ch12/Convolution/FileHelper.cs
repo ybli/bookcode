069527cff2fd997841eb8605555d9cbc5a875554
Li Yingbing <ybli@qq.com>
2019-03-17 21:58:35 +0800

    Part1-ch12

---
+++ Part1-ch12/Convolution/FileHelper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convolution
+{
+    class FileHelper
+    {
+
+        /// <summary>
+        /// 读取M文件
+        /// </summary>
+        /// <param name="file">文件路径</param>
+        /// <returns>所读入的文件内容</returns>
+        public static double[,] ReadM(string file)
+        {
+            int n = 3;
+            var M = new double[n, n];
+            try
+            {
+                var reader = new StreamReader(file);
+                for (int i = 0; i < n; i++)
+                {
+                    string line = reader.ReadLine();
+                    line = line.Replace("	", " ");
+                    var buf = line.Split(' ');
+                    for (int j = 0; j < n; j++)
+                    {
+                        M[i, j] = Convert.ToDouble(buf[j]);
+                    }
+
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return M;
+        }
+        /// <summary>
+        /// 读入N文件
+        /// </summary>
+        /// <param name="file">文件内容</param>
+        /// <returns>文件的内容</returns>
+        public static double[,] ReadN(string file)
+        {
+            int n = 10;
+            var N = new double[n, n];
+            try
+            {
+                var reader = new StreamReader(file);
+                for (int i = 0; i < n; i++)
+                {
+                    string line = reader.ReadLine();
+                    line = line.Replace("	", " ");
+                    var buf = line.Split(' ');
+                    for (int j = 0; j < n; j++)
+                    {
+                        N[i, j] = Convert.ToDouble(buf[j]);
+                    }
+
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return N;
+        }
+        /// <summary>
+        /// 写出计算结果
+        /// </summary>
+        /// <param name="text">计算结果</param>
+        /// <param name="filename">文件路径</param>
+        public static void Write(string text, string filename)
+        {
+            try
+            {
+                var writer=new StreamWriter(filename);
+                writer.WriteLine(text);
+                writer.Close();
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Part1-ch12/Convolution/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Convolution
+{
+    public partial class Form1 : Form
+    {
+        private double[,] M;
+        private double[,] N;
+        private double[,] V1;
+        private double[,] V2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolOpenM_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                M = FileHelper.ReadM(openFileDialog1.FileName);
+
+                int n = 3;
+                string text = Report.Text("N矩阵", M, n, n);
+
+                richTextBox1.Text = text;
+            }
+        }
+
+        private void toolOpenN_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                N = FileHelper.ReadN(openFileDialog1.FileName);
+
+                int n = 10;
+                string text = Report.Text("N矩阵", N, n, n);
+
+                richTextBox1.Text = text;
+            }
+        }
+
+        private void toolCal_Click(object sender, EventArgs e)
+        {
+            Algo go=new Algo(M,N);
+            V1 = go.Algo1();
+            V2 = go.Algo2();
+            int n = 10;
+            string text = Report.Text("算法1结果", V1, n, n);
+            text += Report.Text("算法2结果", V2, n, n);
+
+            richTextBox1.Text = text;
+        }
+
+        private void toolSave_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileHelper.Write(richTextBox1.Text, saveFileDialog1.FileName);
+            }
+        }
+
+        private void toolHelp_Click(object sender, EventArgs e)
+        {
+            string copyright = "《测绘程序设计试题集（试题2 卷积计算）》配套程序\n作者：李英冰\n";
+            copyright += "武汉大学测绘学院\r\nEMAIL: ybli@sgg.whu.edu.cn\r\n2017.7.8";
+            richTextBox1.Text = copyright;
+        }
+    }
+}
